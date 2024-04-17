f63bc48c69fa75fa2a4c717c093b91256d930f6a
rakhafahimshahab <rakha.fahim@ui.ac.id>
2024-04-17 11:08:06 +0700

    Create Subscriber database and Subscriber repository struct skeleton.

---
+++ src/repository/subscriber.rs
@@ -0,0 +1,11 @@
+use crate::model::subscriber::Subscriber;
+use dashmap::DashMap;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref SUBSCRIBERS: DashMap<String, DashMap<String, Subscriber>> = DashMap::new();
+}
+
+pub struct SubscriberRepository;
+
+impl SubscriberRepository {}
